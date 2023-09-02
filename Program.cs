dc03fbc02288071e04adc23e5280b084dd0d481d
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-09-02 14:03:47 +0300

    added search function

--- Program.cs
+++ Program.cs
@@ -3,10 +3,25 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-string[] Input(string massage) 
+string[] Input(string massage)
 {
     Console.Write($"{massage} ");
     string[] input = Console.ReadLine()!.Split(new char[] { ' ', ',' });
     return input;
 }
 
+string[] Search(string[] data)
+{
+    int count = 0;
+    for (int i = 0; i < data.Length; i++)
+    {
+        if (data[i].Length <= 3)
+            count++;
+    }
+    string[] outputArray = new string[count];
+    for (int i = 0; i < data.Length; i++)
+    {
+        if (data[i].Length <= 3)
+            outputArray[i] = data[i];
+    }
+}
